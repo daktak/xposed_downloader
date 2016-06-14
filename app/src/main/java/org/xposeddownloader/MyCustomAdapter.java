0d1dfdecc406960f05cd8f861efe66cb0ec14dcc
daktak <daktak@gmail.com>
2016-06-14 11:19:18 +1000

    Initial commit

---
+++ app/src/main/java/org/xposeddownloader/MyCustomAdapter.java
@@ -0,0 +1,57 @@
+package org.xposeddownloader;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.io.File;
+
+public class MyCustomAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+    private final File[] file;
+
+    public MyCustomAdapter(Context context, String[] values, File[] file) {
+        super(context, R.layout.rowlayout, values);
+        this.context = context;
+        this.values = values;
+        this.file = file;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        ViewHolder holder;
+        String s = values[position];
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.rowlayout, parent, false);
+            holder = new ViewHolder();
+            holder.text = (TextView) convertView.findViewById(R.id.label);
+            convertView.setTag(holder);
+            //Log.w("BasketBuild",s);
+            for (int j = 0; j < file.length; j++) {
+
+                if (s.equals(file[j].getName())) {
+                    //Log.w("BasketBuild","have file: "+s+ ":"+file[j] + " : "+ j+"pos:" + position);
+                    holder.text.setTextColor(R.color.disabledText);
+                    convertView.setEnabled(false);
+                }
+            }
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.text.setText(s);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView text;
+    }
+
+}

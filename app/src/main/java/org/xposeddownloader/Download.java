a68479c5dbfa85697779d5adcbdfc65497902b10
daktak <daktak@gmail.com>
2016-06-17 16:16:07 +1000

    Directory selector

--- app/src/main/java/org/xposeddownloader/Download.java
+++ app/src/main/java/org/xposeddownloader/Download.java
@@ -188,71 +188,79 @@ public class Download extends Service {
         }
     }
     public void download(String url, String desc, String title, String filename) {
-        Log.d(LOGTAG, "Downloading: "+url);
         SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean external = mySharedPreferences.getBoolean("prefExternal",false);
+        String selector = mySharedPreferences.getString("prefSelector", getString(R.string.selector_val)).trim();
+        String exten = selector.substring(selector.lastIndexOf("."), selector.length() - 1);
 
-        String directory = mySharedPreferences.getString("prefDirectory", Environment.DIRECTORY_DOWNLOADS).trim();
-        if (!(directory.startsWith("/"))) {
-            directory = "/" + directory;
-        }
-        File direct = new File(Environment.getExternalStorageDirectory() + directory);
+        if (url.endsWith(exten)) {
 
-        if (!direct.exists()) {
-            direct.mkdirs();
-        }
-        boolean fileExists = false;
+            Log.d(LOGTAG, "Downloading: " + url);
 
-        //check to see if we already have the file
-        //this will make scheduling better
-        if (EasyPermissions.hasPermissions(this, MainActivity.perms2)) {
-            //have to assume we want to download the file if we can't check the dir
-            File f = new File(direct.getAbsolutePath());
-            File file[] = f.listFiles();
-            for (int i = 0; i < file.length; i++) {
-                if (filename.equals(file[i].getName())) {
-                    fileExists = true;
+            boolean external = mySharedPreferences.getBoolean("prefExternal", false);
+
+            String directory = mySharedPreferences.getString("prefDirectory", Environment.DIRECTORY_DOWNLOADS).trim();
+            if (!(directory.startsWith("/"))) {
+                directory = "/" + directory;
+            }
+            File direct = new File(Environment.getExternalStorageDirectory() + directory);
+
+            if (!direct.exists()) {
+                direct.mkdirs();
+            }
+            boolean fileExists = false;
+
+            //check to see if we already have the file
+            //this will make scheduling better
+            if (EasyPermissions.hasPermissions(this, MainActivity.perms2)) {
+                //have to assume we want to download the file if we can't check the dir
+                File f = new File(direct.getAbsolutePath());
+                File file[] = f.listFiles();
+                for (int i = 0; i < file.length; i++) {
+                    if (filename.equals(file[i].getName())) {
+                        fileExists = true;
+                    }
                 }
             }
-        }
 
-        if (!fileExists) {
-            if (external) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            if (!fileExists) {
+                if (external) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                startActivity(intent);
-            } else if (EasyPermissions.hasPermissions(this, MainActivity.perms)) {
-                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-                request.setDescription(desc);
-                request.setTitle(title);
+                    startActivity(intent);
+                } else if (EasyPermissions.hasPermissions(this, MainActivity.perms)) {
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                    request.setDescription(desc);
+                    request.setTitle(title);
 
-                // in order for this if to run, you must use the android 3.2 to compile your app
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                    request.allowScanningByMediaScanner();
-                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                }
+                    // in order for this if to run, you must use the android 3.2 to compile your app
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        request.allowScanningByMediaScanner();
+                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    }
 
-                boolean wifionly = mySharedPreferences.getBoolean("prefWIFI", true);
-                //Restrict the types of networks over which this download may proceed.
-                if (wifionly) {
-                    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
+                    boolean wifionly = mySharedPreferences.getBoolean("prefWIFI", true);
+                    //Restrict the types of networks over which this download may proceed.
+                    if (wifionly) {
+                        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI);
+                    } else {
+                        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+                    }
+                    //Set whether this download may proceed over a roaming connection.
+                    request.setAllowedOverRoaming(false);
+                    request.setDestinationInExternalPublicDir(directory, filename);
+
+                    // get download service and enqueue file
+                    DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                    manager.enqueue(request);
                 } else {
-                    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
+                    Log.d(LOGTAG, "fallout");
                 }
-                //Set whether this download may proceed over a roaming connection.
-                request.setAllowedOverRoaming(false);
-                request.setDestinationInExternalPublicDir(directory, filename);
-
-                // get download service and enqueue file
-                DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-                manager.enqueue(request);
             } else {
-                Log.d(LOGTAG, "fallout");
+                Log.d(LOGTAG, "file-exists");
             }
         } else {
-            Log.d(LOGTAG, "file-exists");
+            Log.d(LOGTAG, "Not downloading: " + url);
         }
     }
-
 }
--- app/src/main/java/org/xposeddownloader/PrefsFragment.java
+++ app/src/main/java/org/xposeddownloader/PrefsFragment.java
@@ -1,6 +1,8 @@
 package org.xposeddownloader;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 
@@ -16,6 +18,25 @@ public class PrefsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings);
+
+        Preference prefDir = findPreference("prefDirectory");
+
+        //prefDir.setIntent(i);
+        prefDir.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent i = new Intent(getActivity(), DirectoryPicker.class);
+                /* If you set default dir , you can't navigate up!
+                SharedPreferences mySharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String dir = mySharedPreferences.getString("prefDirectory",null);
+                i.putExtra(DirectoryPicker.START_DIR, dir);*/
+
+                startActivity(i);
+                return true;
+            }
+        });
+
+
     }
 
 }
